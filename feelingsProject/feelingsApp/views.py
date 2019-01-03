76b17114ccec624721f45d73a0e1f214add87b3f
Jawann Carmona <jcarmona@ACTNY-IT-Jawann-Carmona.local>
2019-01-03 11:57:50 -0500

    changed form to be textarea. used "text-processing.com" to add sentiment analysis upon fomr submission

--- feelingsProject/feelingsApp/views.py
+++ feelingsProject/feelingsApp/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 
 
 def index(request):
+
     question = Question.objects.get(pk=1)
     output = question
     choices = question.choice_set.all()
@@ -15,6 +16,7 @@ def index(request):
         'question': question,
         'choices': choices
     }
+
     return render(request, 'feelingsApp/index.html', context)
 
 
@@ -23,11 +25,29 @@ def question_list(request):
 
 
 def vote(request):
-    question = Question.objects.get(pk=1)
-    output = question
-    choices = question.choice_set.all()
-    context = {
-        'question': question,
-        'choices': choices
-    }
-    return render(request, 'feelingsApp/detail.html', context)
+    if request.method == "POST":
+        print(request.POST.get('text'))
+        text = request.POST.get('text')
+
+        import http.client
+
+        conn = http.client.HTTPConnection("text-processing.com")
+
+        payload = "text={}".format(text).encode('utf-8')
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'cache-control': "no-cache",
+            'postman-token': "d7009e69-4162-2afa-df9c-15683ef75510"
+        }
+
+        conn.request("POST", "/api/sentiment/", payload, headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        print(data.decode("utf-8"))
+        context = {"nlp": data.decode("utf-8")}
+
+        return render(request, 'feelingsApp/detail.html', context)
+
+    return render(request, 'feelingsApp/detail.html')
